2837a19ae7087afc07738a815e4c60c515760d35
mariogmarq <mariogarciamarq@gmail.com>
2020-10-13 17:10:27 +0200

    Empezado reto 2

---
+++ retos/reto2/cola.h
@@ -0,0 +1,68 @@
+/**
+ * @file cola.h
+ * @author Mario García Márquez y Daniel Alconchel Vázquez
+ * @date 13 de Octubre de 2020
+ */
+
+#ifndef COLA_H
+#define COLA_H
+#include "ficha.h"
+
+/**
+ * @brief T.D.A. Cola
+ * 
+ * Una instancia del tipo de dato abstracto cola es un objeto que contiene las 4 
+ * siguientes fichas a jugar
+ */
+class cola
+{
+private:
+    /**
+ * @brief lista enlazada para representar la cola
+ */
+    struct elemento
+    {
+        ficha valor;
+        elemento *siguiente;
+    };
+
+    /**
+         * @brief representacion interna de la cola como una lista enlazada cerrada
+         * @note el ultimo elemento de la lista apunta al primer elemento de esta
+         * como el tamaño de la lista siempre es 4 elementos nos aporta una mayor facilidad a la hora
+         * de extraer fichas
+         */
+    elemento lista;
+
+public:
+    /**
+     * @brief constructor de cola
+     * @note genera 4 fichas aleatorias y las mete en la cola
+     */
+    cola();
+
+    /**
+     * @brief constructor copia
+     * @param orig instancia clase cola
+     */
+    cola(const cola &orig);
+
+    /**
+     * @brief destructor de la clase
+     */
+    ~cola();
+
+    /**
+     * @brief devuelve un vector con las componentes de la lista
+     * @return std::vector con todas las componentes de la lista
+     */
+    std::vector<ficha> getFichas() const;
+
+    /**
+     * @brief devuelve el primer elemento de la lista
+     * @return la primera ficha de la lista
+     */
+    ficha sacarFicha();
+};
+
+#endif
---
+++ retos/reto2/ficha.h
@@ -0,0 +1,86 @@
+/**
+ * @file ficha.h
+ * @author Mario García Márquez y Daniel Alconchel Vázquez
+ * @date 13 de Octubre de 2020
+ */
+
+#ifndef FICHA_H
+#define FICHA_H
+#include <vector>
+
+/**
+ * @brief T.D.A. Ficha
+ * 
+ * Una instancia del tipo de dato abstracto Ficha es un objeto que representa una 
+ * ficha del tetris. Está representado por una forma seleccionada de un enum.
+ */
+class ficha
+{
+
+public:
+    /**
+     * @brief enum indicando el tipo de ficha
+     */
+    enum tipo
+    {
+        vacio,
+        recta,
+        cuadrado,
+        t,
+        lDerecha,
+        lIzquierda,
+        zDerecha,
+        zIzquierda
+    };
+
+    /**
+     * @brief constructor base
+     */
+    ficha();
+
+    /**
+     * @brief constructor copia
+     * @param orig instancia clase ficha
+     */
+    ficha(const ficha &orig);
+
+    /**
+     * @brief constructor inializando la forma
+     * @param forma tipo de la ficha
+     * @note se recomienda usar
+     */
+    ficha(ficha::tipo forma);
+
+    /**
+     * @brief destructor de la clase
+     */
+    ~ficha();
+
+    /**
+     * @brief establece la forma de la ficha
+     * @param forma la forma a establecer
+     */
+    void setForma(ficha::tipo forma);
+
+    /**
+     * @brief devuelve la forma establecida
+     * @return la forma de la ficha
+     */
+    tipo getForma() const;
+
+    /**
+     * @brief devuelve la matriz asociada a la forma de la ficha
+     * @return la matriz asociada a la forma de la ficha
+     * @note en la matriz solo se marcan las casillas ocupadas y las no ocupadas tendran el tipo tipo::vacio
+     * @see ficha::tipo 
+     */
+    std::vector<std::vector<ficha::tipo>> getMatriz() const;
+
+private:
+    /**
+ * @brief forma de la ficha
+ */
+    tipo forma;
+};
+
+#endif

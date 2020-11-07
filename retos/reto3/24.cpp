9a94b65b00460956fe21715e60ee8f7da1d0e09c
mariogmarq <mariogarciamarq@gmail.com>
2020-11-07 18:16:43 +0100

    24 reto 3 arreglado

--- retos/reto3/24.cpp
+++ retos/reto3/24.cpp
@@ -17,27 +17,34 @@ using namespace std;
 void postintercalar(list<int>& l1, list<int>& l2) {
   // Miramos los casos en los que una lista es vacia
   if (l2.empty()) {
-    l2.swap(l1);
+    for (auto i = l1.begin(); i != l1.end(); ++i) l2.push_front(*i);
+    l1.clear();
     return;
   }
   if (l1.empty()) {
     return;
   }
+  // Nos ponemos en la ultima posicion con un iterador, no usamos rbegin ya que
+  // no funciona con la funcion insert
+  auto dondeInsertar = --(l2.end());
 
-  auto dondeInsertar = l2.begin();
-
+  // mientras queden elementos en l1 los insertaremos en el iterador
   while (!l1.empty()) {
     l2.insert(dondeInsertar, l1.front());
     l1.pop_front();
-    if (dondeInsertar != l2.end()) ++dondeInsertar;
+    // nos movemos dos posiciones a la derecha sin pasarnos de begin
+    if (dondeInsertar != l2.begin()) {
+      --dondeInsertar;
+      if (dondeInsertar != l2.begin()) --dondeInsertar;
+    }
   }
 }
 
 int main() {
-  int l1[] = {100, 200};
+  int l1[] = {100, 200, 300};
   int l2[] = {1, 2, 3, 4, 5, 6};
-  list<int> lis1(l1, l1 + 2);
-  list<int> lis2(l2, l2 + 6);
+  list<int> lis2(l1, l1 + 3);
+  list<int> lis1(l2, l2 + 6);
   postintercalar(lis1, lis2);
   for (auto i = lis2.begin(); i != lis2.end(); ++i) cout << "  " << *i;
 }

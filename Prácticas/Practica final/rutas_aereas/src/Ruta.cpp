67b6007830d187365c9fbc883a2cc367b95b302f
mariogmarq <mariogarciamarq@gmail.com>
2020-12-30 16:07:58 +0100

    Arreglado puntos interes

--- Prácticas/Practica final/rutas_aereas/src/Ruta.cpp
+++ Prácticas/Practica final/rutas_aereas/src/Ruta.cpp
@@ -7,95 +7,101 @@
 #include <algorithm>
 
 Ruta::Ruta() {
-    paises = nullptr;
-    nombre = " ";
-    recorrido.clear();
-    interes.clear();
+  paises = nullptr;
+  nombre = " ";
+  recorrido.clear();
+  interes.clear();
 }
 
-Ruta::Ruta(Paises *paises) {
-    this->paises = paises;
-}
+Ruta::Ruta(Paises *paises) { this->paises = paises; }
 
 void Ruta::nuevoPunto(Punto punto) {
-    if (recorrido.size() == 0)
-        recorrido.push_back(punto);
-    else {
-        bool ya_esta = false;
-        for (long unsigned int i = 0; i < recorrido.size(); i++)
-            if (recorrido[i].operator==(punto))
-                ya_esta = true;
-        if (!ya_esta) {
-            std::vector <Punto> aux;
-            bool insertado = false;
-            for (long unsigned int i = 0; i < recorrido.size(); i++) {
-                if (!recorrido[i].operator<(punto)) {
-                    if (!insertado) {
-                        aux.push_back(punto);
-                        insertado = true;
-                    }
-                }
-                aux.push_back(recorrido[i]);
-            }
-            if(!insertado)
-                aux.push_back(punto);
-            recorrido.clear();
-            recorrido = aux;
+  if (recorrido.size() == 0)
+    recorrido.push_back(punto);
+  else {
+    bool ya_esta = false;
+    for (long unsigned int i = 0; i < recorrido.size(); i++)
+      if (recorrido[i].operator==(punto))
+        ya_esta = true;
+    if (!ya_esta) {
+      std::vector<Punto> aux;
+      bool insertado = false;
+      for (long unsigned int i = 0; i < recorrido.size(); i++) {
+        if (!recorrido[i].operator<(punto)) {
+          if (!insertado) {
+            aux.push_back(punto);
+            insertado = true;
+          }
         }
+        aux.push_back(recorrido[i]);
+      }
+      if (!insertado)
+        aux.push_back(punto);
+      recorrido.clear();
+      recorrido = aux;
     }
+  }
 }
 
-void Ruta::nuevoNombre(std::string nombre) {
-    this->nombre= nombre;
-}
+void Ruta::nuevoNombre(std::string nombre) { this->nombre = nombre; }
 
 void Ruta::nuevoInteres(Punto punto) {
-    bool ya_esta = false;
-    for(long unsigned int i = 0; i < interes.size(); i++)
-        if(interes[i].operator==(punto))
-            ya_esta = true;
-    if(!ya_esta)
-        interes.push_back(punto);
+  bool ya_esta = false;
+  for (long unsigned int i = 0; i < interes.size(); i++)
+    if (interes[i].operator==(punto))
+      ya_esta = true;
+  if (!ya_esta)
+    interes.push_back(punto);
 }
 
-int Ruta::longitud() const {
-    return this->recorrido.size();
-}
+int Ruta::longitud() const { return this->recorrido.size(); }
 
-int Ruta::cantidadInteres() const {
-    return this->interes.size();
-}
+int Ruta::cantidadInteres() const { return this->interes.size(); }
 
-std::string Ruta::GetNombre() const {
-    return this->nombre;
-}
+std::string Ruta::GetNombre() const { return this->nombre; }
 
-std::istream& operator>>(std::istream& is, Ruta& r){
-    // Leemos comentario
-    if (is.peek() == '#') {
-        std::string a;
-        getline(is, a);
-    }
+std::istream &operator>>(std::istream &is, Ruta &r) {
+  // Leemos comentario
+  if (is.peek() == '#') {
+    std::string a;
+    getline(is, a);
+  }
 
-    std::string b;
-    is >> b;
-    r.nuevoNombre(b);
+  std::string b;
+  is >> b;
+  r.nuevoNombre(b);
+  int c;
+  is >> c;
+  for (int i = 0; i < c; i++) {
+    char basura;
+    float lat, lon;
+    is >> basura >> lat >> basura >> lon >> basura;
+    r.nuevoPunto(Punto(lat, lon));
+  }
+
+  // Comprobamos si hay puntos de interes
+  if (is.peek() == '#') {
+    std::string a;
+    is >> a;
     int c;
     is >> c;
-    for (int i = 0; i<c; i++){
-        char basura;
-        float lat, lon;
-        is >> basura >> lat >> basura >> lon >> basura;
-        r.nuevoPunto(Punto(lat, lon));
+    for (int i = 0; i < c; i++) {
+      char basura;
+      float lat, lon;
+      is >> basura >> lat >> basura >> lon >> basura;
+      r.nuevoInteres(Punto(lat, lon));
     }
-    return is;
+  }
+
+  return is;
 }
 
-std::ostream& operator<<(std::ostream& os, const Ruta& r){
-    os << r.GetNombre() << " " << r.longitud() << " ";
-    for (int i=0; i<r.longitud(); i++)
-        os<< "(" << r.recorrido[i].GetLatitud() << "," << r.recorrido[i].GetLongitud() <<")" << " ";
-    os << std::endl;
-    return os;
+std::ostream &operator<<(std::ostream &os, const Ruta &r) {
+  os << r.GetNombre() << " " << r.longitud() << " ";
+  for (int i = 0; i < r.longitud(); i++)
+    os << "(" << r.recorrido[i].GetLatitud() << ","
+       << r.recorrido[i].GetLongitud() << ")"
+       << " ";
+  os << std::endl;
+  return os;
 }
-
